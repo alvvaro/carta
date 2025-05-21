0f7a069131a59965ec469f0670813bc062fe79c7
Álvaro González Serna <50526113+alvvaro@users.noreply.github.com>
2025-05-21 23:36:54 +0200

    checkpoint

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,51 +2,59 @@ import Card from '@/components/common/Card';
 import SearchBar from '@/components/search/SearchBar';
 import useSearch from '@/hooks/useSearch';
 
+import Row from '../layout/Row';
+
 export default function Search() {
-  const { results, isLoading, isValidating } = useSearch();
+  const { results } = useSearch();
 
   return (
     <>
       <SearchBar />
 
-      <div className="p-3">Results:</div>
-      <div className="p-3">{isLoading ? 'isLoading' : 'not loading'}</div>
-      <div className="p-3">
-        {isValidating ? 'isValidating' : 'not validating'}
-      </div>
-
-      {results ?
-        <div className="">
-          {results.programs?.page?.items?.map((item) => (
+      {results?.programs?.page.items.length ?
+        <Row title="Programas">
+          {results.programs.page.items.map((item) => (
             <Card
-              horizontal
               key={item.id}
               img={item.imgPortada || item.imgBanner}
-              href="#"
+              href={`${item.contentType}/${item.id}`}
+              pretitle={item.productionDate || undefined}
               title={item.title}
+              subtitle={item.promoText || undefined}
             />
           ))}
-          {results.contents?.page?.items?.map((item) => (
+        </Row>
+      : null}
+
+      {results?.videos?.page.items.length ?
+        <Row title="Videos">
+          {results.videos.page.items.map((item) => (
             <Card
-              horizontal
               key={item.id}
               img={item.thumbnail as string}
-              href="#"
+              href={`${item.contentType}/${item.id}`}
+              pretitle={item.dateOfEmission}
               title={item.title}
+              subtitle={item.programInfo.title}
+              duration={item.duration}
             />
           ))}
-          {results.videos?.page?.items?.map((item) => (
+        </Row>
+      : null}
+
+      {results?.contents?.page.items.length ?
+        <Row title="Contenidos">
+          {results.contents.page.items.map((item) => (
             <Card
-              horizontal
               key={item.id}
               img={item.thumbnail as string}
-              href="#"
+              href={`${item.contentType}/${item.id}`}
               pretitle={item.programInfo.title}
               title={item.title}
-              subtitle={item.dateOfEmission}
+              duration={item.duration}
             />
           ))}
-        </div>
+        </Row>
       : null}
     </>
   );
